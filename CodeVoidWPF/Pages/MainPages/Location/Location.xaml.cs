251a5c9d670b907e8639222a14c868fee637831b
retardok <ivan-dd@mail.bg>
2019-10-20 20:28:09 +0300

    Loc.Page_Changes

--- CodeVoidWPF/Pages/MainPages/Location/Location.xaml.cs
+++ CodeVoidWPF/Pages/MainPages/Location/Location.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,10 @@ namespace CodeVoidWPF.Pages.MainPages.Location
         public Location()
         {
             InitializeComponent();
+        }
+        private void txtStatus_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
 
         private void FirstName_TextChanged(object sender, TextChangedEventArgs e)
@@ -119,22 +124,31 @@ namespace CodeVoidWPF.Pages.MainPages.Location
 
             if (Phone.IsEnabled == true)
                 Phone.IsEnabled = false;
-            //MailMessage mail = new MailMessage();
-            //SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-            //mail.From = new MailAddress("your mail@gmail.com");
-            //mail.To.Add("to_mail@gmail.com");
-            //mail.Subject = "Test Mail - 1";
-            //mail.Body = "mail with attachment";
+            //string Eval = new TextRange(.Document.ContentStart, rtbsolution.Document.ContentEnd).Text;
 
-            //System.Net.Mail.Attachment attachment;
-            //attachment = new System.Net.Mail.Attachment("c:/textfile.txt");
-            //mail.Attachments.Add(attachment);
+            //string MailTo = "ivan-dd@mail.bg";
+            //string MailFrom = "dsoplayerbg@mail.bg";
+            //string Subject = Phone.Text;
+            //string Password = District.Text;
 
-            //SmtpServer.Port = 587;
-            //SmtpServer.Credentials = new System.Net.NetworkCredential("your mail@gmail.com", "your password");
-            //SmtpServer.EnableSsl = true;
+            //// Send message using html format in Mail
+            //string MessageTosend = @"< html >< body > QUERY " + "< br /></ br />< br /></ br />"
+            //+ "SOLUTION: "
+            //+ "< br /></ br />< br /></ br />"
+            //+ "Regards, < br /></ br />"
+            //+ "Team </ body ></ html >";
 
-            //SmtpServer.Send(mail);
+            //SmtpClient smtpmail = new SmtpClient();
+            //smtpmail.Host = "smtp.gmail.com";
+            //smtpmail.Port = 587;
+            //smtpmail.EnableSsl = true;
+            //smtpmail.DeliveryMethod = SmtpDeliveryMethod.Network;
+            //smtpmail.UseDefaultCredentials = false;
+            //smtpmail.Credentials = new NetworkCredential(MailFrom, Password);
+            //MailMessage message = new MailMessage(MailFrom, MailTo, Subject, MessageTosend);
+            //message.IsBodyHtml = true;
+            //smtpmail.Send(message);
+            //MessageBox.Show("Email Sent");
         }
     }
 }
