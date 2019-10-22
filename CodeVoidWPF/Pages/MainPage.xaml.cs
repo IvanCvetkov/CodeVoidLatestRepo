f751b5830bf3e54117bc248759592e34a9329dbd
retardok <ivan-dd@mail.bg>
2019-10-22 22:14:11 +0300

    AnimationsMainPage

--- CodeVoidWPF/Pages/MainPage.xaml.cs
+++ CodeVoidWPF/Pages/MainPage.xaml.cs
@@ -1,21 +1,14 @@
-﻿using Microsoft.CSharp;
-using System.CodeDom.Compiler;
-using System.Diagnostics;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Data;
-using System.Reflection;
-using System.IO;
 using System;
-using System.Windows.Documents;
+using System.Windows.Media.Animation;
 
 namespace CodeVoidWPF.Pages
 {
     /// <summary>
     /// Interaction logic for MainPage.xaml
     /// </summary>
-    
+
     public partial class MainPage : Page
     {
         public MainPage()
@@ -38,5 +31,44 @@ namespace CodeVoidWPF.Pages
             this.NavigationService.Navigate(new Uri("Pages/MainPages/Location/Location.xaml", UriKind.Relative));
         }
 
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            BlueGridMethod();
+            RectangleMethod();
+            PurpleGridMethod();
+        }
+        public void BlueGridMethod()
+        {
+            DoubleAnimation db = new DoubleAnimation();
+            db.From = 0;
+            db.To = 1196;
+            db.Duration = TimeSpan.FromSeconds(1);
+
+            db.EasingFunction = new QuinticEase();
+
+            BlueGrid.BeginAnimation(WidthProperty, db);
+        }
+        public void RectangleMethod()
+        {
+            DoubleAnimation db = new DoubleAnimation();
+            db.From = 0;
+            db.To = 1196;
+            db.Duration = TimeSpan.FromSeconds(1);
+
+            db.EasingFunction = new QuinticEase();
+
+            Rec.BeginAnimation(WidthProperty, db);
+        }
+        public void PurpleGridMethod()
+        {
+            DoubleAnimation db = new DoubleAnimation();
+            db.From = 0;
+            db.To = 1196;
+            db.Duration = TimeSpan.FromSeconds(1);
+
+            db.EasingFunction = new QuinticEase();
+
+            PurpleGrid.BeginAnimation(WidthProperty, db);
+        }
     }
 }
